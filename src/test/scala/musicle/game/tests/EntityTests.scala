8f5623caa17002e3aba86cd08ab57731668fa1fd
Kresten Laust <krestenlaust@gmail.com>
2025-02-08 14:36:45 +0100

    Define json codecs for entity types
    - Doesn't define a type for the attribute map

--- src/test/scala/musicle/game/tests/EntityTests.scala
+++ src/test/scala/musicle/game/tests/EntityTests.scala
@@ -2,6 +2,8 @@ package musicle.game.test
 
 import musicle.game.*
 import org.scalatest.flatspec.AnyFlatSpec
+import io.bullet.borer.Json
+import io.bullet.borer.syntax._
 
 class EntityTests extends AnyFlatSpec {
   "Entity attributes" should "be accessible" in {
@@ -16,6 +18,29 @@ class EntityTests extends AnyFlatSpec {
 
     val actualTitleAttribute = entity.getAttribute[TitleAttribute]("title")
     
+    assert(actualTitleAttribute.isDefined === true)
+    assert(actualTitleAttribute.get === TitleAttribute("Runaway"))
+  }
+
+  "Entity attributes" should "be parsed from JSON" in {
+    val jsonString =
+        """
+            {
+            "category": "music",
+            "title": "Runaway",
+            "artist": "Aurora",
+            "music_uri": "https://youtube.com/...",
+            "lyrics_sample": "I was listening to the ocean...",
+            "release_year": 2015,
+            "album": "All My Demons Greeting Me as a Friend"
+            }
+        """
+    val decoder = Json.parse(jsonString).to[Entity]
+
+    assert(decoder.isDefined === true)
+    val entity = decoder.get
+
+    val actualTitleAttribute = entity.getAttribute[TitleAttribute]("title")
     assert(actualTitleAttribute.isDefined === true)
     assert(actualTitleAttribute.get === TitleAttribute("Runaway"))
   }
